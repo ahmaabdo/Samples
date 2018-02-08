279c1ef7f25903020a28e1fb45f16912502c74e8
Ahmad ‫‪‪‪ <a7maabdo@gmail.com>
2018-02-08 22:48:45 +0200

    Order Activity

--- HeadFirst/BitsandPizzas/app/src/main/java/ahmaabdo/bitsandpizzas/OrderActivity.java
+++ HeadFirst/BitsandPizzas/app/src/main/java/ahmaabdo/bitsandpizzas/OrderActivity.java
@@ -1,8 +1,11 @@
 package ahmaabdo.bitsandpizzas;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -13,5 +16,19 @@ public class OrderActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Code that runs when the FAB is clicked
+    public void onClickDone(View view) {
+        Snackbar.make(findViewById(R.id.coordinator),
+                getResources().getString(R.string.update_order),
+                Snackbar.LENGTH_LONG)
+                .setAction(getResources().getString(R.string.undo), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Code to run when the user clicks on the Undo action
+                        Toast.makeText(OrderActivity.this, R.string.order_undone, Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
     }
 }
